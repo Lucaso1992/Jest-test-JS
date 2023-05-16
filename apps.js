f2023f9a80d1d78cdd04d7a290153bfca258dd99
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-16 19:29:04 +0000

    modificacion

--- apps.js
+++ apps.js
@@ -5,3 +5,13 @@ const sum = (a, b) => {
 console.log(sum);
 
 module.exports = { sum };
+
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+const converEuroToDollar = (monto) => monto *1.2;
+const convertEuroToJPY = 
